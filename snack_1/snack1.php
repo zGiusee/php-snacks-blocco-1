cbc67e420557b3521154d6e371fc9ef2011454bc
zGiusee <giusebelforte@gmail.com>
2024-01-24 14:52:49 +0100

    [UPDATE] Snack 1 done

--- snack_1/snack1.php
+++ snack_1/snack1.php
@@ -1,7 +1,7 @@
 <?php
 
 // Dfinisco l'array delle partite disputate
-$match = [
+$matches = [
     [
         'home' => 'Atlanta Hawks',
         'guest' => 'Oklahoma City Thunder',
@@ -56,8 +56,30 @@ $match = [
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="my-bg-black">
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-white fw-bold pt-3">Risultati delle partite della 4' Giornata:</h1>
+                </div>
+                <!--CICLO (PER CICLARE GLI OGGETTI DENTRO L'ARRAY) -->
+                <?php foreach ($matches as $match) {  ?>
 
+                    <div class="col-6 py-5">
+
+                        <div class="match_info_container">
+                            <h4 class=" text-center fw-bold   "><?php echo $match['home']." - ".$match['guest'];  ?></h4>
+                            <h5 class="text-center fw-bold py-1"> Finale: </h5>
+                            <p class=" text-center"><?php echo $match['home_points']." - ".$match['guest_points'];  ?></p>
+                        </div>
+
+                    </div>
+
+                <?php } ?>
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
