66ace410b1de90b7b465d3c9570ff700012e30de
zGiusee <giusebelforte@gmail.com>
2024-01-24 15:07:47 +0100

    [UPDATE] Snack 3 array done.

--- snack_3/snack3.php
+++ snack_3/snack3.php
@@ -1,5 +1,56 @@
 <?php
+// Dfinisco l'array degli articoli
+$articles = [
 
+    '23/04/2024' => [
+        [
+            'fonte' => 'TGCOM 24',
+            'title' => 'Maxi arresto!',
+            'author' => 'Michele Giagiugi',
+            'text' => 'Incredibile arrestati tutti quelli che lasciano il visualizzato su WhatsApp!'
+        ],
+        [
+            'fonte' => 'TGCOM 25',
+            'title' => 'Guerra nel villagio della Foglia!',
+            'author' => 'Naruto',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '24/04/2024' => [
+        [
+            'fonte' => 'TGCOM 24',
+            'title' => 'Limiti di velocità, interviene il ministero: "È arbitrario generalizzarli in città" ',
+            'author' => 'Ricky Mattei',
+            'text' => 'Dopo le ordinanze del Comune di Bologna che istituiscono in città il limite di 30 km/h, la direttiva precisa che l abbassamento può essere deciso solo in aree limitate e per specifici motivi'
+        ]
+    ],
+    '25/04/2024' => [
+        [
+            'fonte' => 'TGCOM 24',
+            'title' => 'Fleximan colpisce ancora in Veneto, i sindaci si arrendono: "Non ripareremo gli autovelox abbattuti"',
+            'author' => 'V per Vendetta',
+            'text' => 'Questa volta è comparsa anche la firma: un cartello con la scritta a caratteri cubitali "Fleximan sta arrivando"'
+        ],
+        [
+            'fonte' => 'TGCOM 26',
+            'title' => 'Una super-telecamera "vede" i colori con gli occhi degli animali ',
+            'author' => 'Leone il cane fifone',
+            'text' => 'Boh ok, ci sta.'
+        ],
+        [
+            'fonte' => 'TGCOM 24',
+            'title' => 'Sinner supera Rublev: semifinale con Djokovic ',
+            'author' => 'Marco Cangelli',
+            'text' => 'Il 22enne di Sesto Pusteria si è imposto per 6-4 7-6 6-3 al termine di una sfida durata quasi tre ore ritrovando così il serbo al prossimo turno'
+        ],
+        [
+            'fonte' => 'TGCOM 23',
+            'title' => 'Matera: arrestato un 57enne! Ecco che cosa ha scoperto la Polizia',
+            'author' => 'Seth Rollins',
+            'text' => 'Non gli piacevano i sassi.'
+        ]
+    ],
+];
 ?>
 
 <!DOCTYPE html>
